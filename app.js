cc854dc9df24d12f85c70ad0a64a3f6b9cfa77b4
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-15 14:15:58 -0700

    more classes

--- app.js
+++ app.js
@@ -8,5 +8,37 @@ class Alligator {
     this.color = color;
     }
 }
-
 let ally = new Alligator ("Snapper", "green");
+
+class Buffalo {
+    constructor(
+        name = '',
+        color = '',
+){
+    this.name = name;
+    this.color = color;
+    }
+}
+let beast = new Buffalo ("Reiner", "Brown");
+
+class Canary {
+    constructor(
+        name = '',
+        color = '',
+){
+    this.name = name;
+    this.color = color;
+    }
+}
+let bird = new Canary("George","Yellow")
+
+class Dolphin {
+    constructor(
+        name = '',
+        color = '',
+){
+    this.name = name;
+    this.color = color;
+    }
+}
+let seaCreature = new Dolphin ("","")
