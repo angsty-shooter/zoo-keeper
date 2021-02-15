d1b09f23069e722dec7531f8ef4cf075a9cefb82
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-15 14:56:14 -0700

    all the pokemon

--- app.js
+++ app.js
@@ -141,3 +141,146 @@ class Magikarp {
     }
 }
 let uselessFish = new Magikarp ("Water","Orange,Yellow")
+
+class Nidoqueen {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let scaryRhino = new Nidoqueen ("Poison,Ground","Blue,Yellow")
+
+class Onix {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let rockSnek = new Onix ("Rock,Ground","Gray")
+
+class Pangoro {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let panda = new Pangoro ("Fighting,Dark","White,Black")
+
+class Quagsire {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let bloop = new Quagsire ("Water,Ground","Blue")
+
+class Raichu {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let sparkMouse = new Raichu ("Electric","Orange,Yellow,Black")
+
+class Scizor {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let bigBug = new Scizor ("Bug,Steel","Red,Black")
+
+class Taillow {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let tinyBirb = new Taillow ("Normal,FLying","Red,White,Blue")
+
+class Umbreon {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let darkCat = new Umbreon ("Dark","Black,Yellow")
+
+class Vanillite {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let iceCream = new Vanillite ("Ice","White,Blue")
+
+class Wingull {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let seaBird = new Wingull ("Water,Flying","White,Blue")
+
+class Xerneus {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let magicElk = new Xerneus ("Fairy","Black,Blue,Yellow")
+
+class Yveltal {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let yBoi = new Yveltal ("Dark,Flying","Black,Red")
+
+class Zekrom {
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let scaryDragon = new Zekrom ("Dragon,Electric","Black")
