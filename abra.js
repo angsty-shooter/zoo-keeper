39914ddfdd9bc212b045dd734e112a86cc23dbdd
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-15 15:07:00 -0700

    renamed js file

---
+++ abra.js
@@ -0,0 +1,11 @@
+
+class Abra{
+    constructor(
+        type = '',
+        color = '',
+){
+    this.type = type;
+    this.color = color;
+    }
+}
+let ally = new Abra ("Psychic","Yellow,Brown");
